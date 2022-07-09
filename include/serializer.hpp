2d5ec5c419030e06fe76853bfb18f9e500938258
Luc Sinet <luc.sinet@gmail.com>
2022-07-09 18:10:39 +0200

    feat: serialize Entry<T> & Entry<tuple>

---
+++ include/serializer.hpp
@@ -0,0 +1,33 @@
+#ifndef CPPDICT_SERIALIZER_HPP
+#define CPPDICT_SERIALIZER_HPP
+
+#include <tuple>
+#include <utility>
+
+#include "entry.hpp"
+
+template <auto Writer> class Serializer {
+public:
+  template <typename Entry, typename... Args>
+  void serialize(const Entry &entry, const Args &...args) {
+    this->serializeEntry(entry);
+    if constexpr (sizeof...(args) > 0) {
+      this->serialize(args...);
+    }
+  }
+
+private:
+  template <typename... Args>
+  void serializeEntry(const Entry<std::tuple<Args...>> &entry) {
+    Writer.writeStartElement(entry.name);
+    std::apply([this](const auto &...entries) { this->serialize(entries...); },
+               entry.value);
+    Writer.writeEndElement();
+  }
+
+  template <typename T> void serializeEntry(const Entry<T> &entry) {
+    Writer.write(entry.name, entry.value);
+  }
+};
+
+#endif // !CPPDICT_SERIALIZER_HPP
